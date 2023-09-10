a92a03d6b4d260a5387c36c4ff7ac25359d8356e
vatsalya <vatsalyasharma24@gmail.com>
2023-09-10 23:13:07 +0530

    commit

---
+++ ghrce_admin.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    cout<<"**********---Welcome to Raisoni Group---**********\n";
+    cout<<"1. Register new Admission\n";
+    cout<<"2. Search detail of the student\n";
+    cout<<"3. Display criteria for admission\n";
+    cout<<"4. quit";
+    int select;
+    cout<<"Enter the number required operations\n";
+    cin>>select;
+    switch(select)
+
+
+
+return 0;
+}
